18f5590ac0c5fe7412a414656612f8c63f99f098
llwowowowoll <1226227629@qq.com>
2016-04-03 13:38:36 +0800

    Problem19 镜像二叉树
    递归

---
+++ 19 二叉树镜像/Solution.java
@@ -0,0 +1,31 @@
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    public void Mirror(TreeNode root) {
+        if(root==null){
+        	return;
+        }
+    	TreeNode tmp = null;
+        tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        
+        if(root.left!=null){
+        	Mirror(root.left);
+        }
+        if(root.right!=null){
+        	Mirror(root.right);
+        }
+    }
+}
